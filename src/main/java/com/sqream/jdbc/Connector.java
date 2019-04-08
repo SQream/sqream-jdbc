e36bf2d311d965f3a468dd9dd557c8fef7297c69
eliy <eliy@sqreamtech.com>
2019-04-08 14:07:05 +0300

    opened ftInt / ftDouble sanitation for spotfire support

--- src/main/java/com/sqream/jdbc/Connector.java
+++ src/main/java/com/sqream/jdbc/Connector.java
@@ -947,7 +947,7 @@ public class Connector {
     public Short get_short(int col_num) throws ConnException {   col_num--;  // set / get work with starting index 1
         
     	if (col_types[col_num].equals("ftUByte"))
-            return (null_columns[col_num] == null || null_columns[col_num][row_counter] == 0) ? (short)data_columns[col_num].get() : null;
+            return (null_columns[col_num] == null || null_columns[col_num][row_counter] == 0) ? (short)(data_columns[col_num].get() & 0xFF) : null;
 
 		return (_validate_get(col_num, "ftShort")) ? data_columns[col_num].getShort() : null;
     }
@@ -955,9 +955,11 @@ public class Connector {
     
     public Integer get_int(int col_num) throws ConnException {   col_num--;  // set / get work with starting index 1
         
-    if (col_types[col_num].equals("ftShort"))
-        return (null_columns[col_num] == null || null_columns[col_num][row_counter] == 0) ? (int)data_columns[col_num].getShort() : null;
-        
+	    if (col_types[col_num].equals("ftShort"))
+	        return (null_columns[col_num] == null || null_columns[col_num][row_counter] == 0) ? (int)data_columns[col_num].getShort() : null;
+	    else if (col_types[col_num].equals("ftUByte"))
+	        return (null_columns[col_num] == null || null_columns[col_num][row_counter] == 0) ? (int)(data_columns[col_num].get() & 0xFF) : null;
+
         return (_validate_get(col_num, "ftInt")) ? data_columns[col_num].getInt() : null;
         //return (null_balls[col_num].get() == 0) ? data_columns[col_num].getInt() : null;
     }
@@ -977,6 +979,9 @@ public class Connector {
     
     public Double get_double(int col_num) throws ConnException {   col_num--;  // set / get work with starting index 1
         
+	    if (col_types[col_num].equals("ftFloat"))
+	        return (null_columns[col_num] == null || null_columns[col_num][row_counter] == 0) ? (double)data_columns[col_num].getFloat() : null;
+
         return (_validate_get(col_num, "ftDouble")) ? data_columns[col_num].getDouble() : null;
     }
     

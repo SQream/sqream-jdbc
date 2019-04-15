ca27f4d8aac9d86c0a6deb250060b1acab2ba406
eliy <eliy@sqreamtech.com>
2019-04-15 14:25:51 +0300

    prettified test

--- jdbc.py
+++ jdbc.py
@@ -1,17 +1,19 @@
 import os, sys
-import py4j
 from py4j.java_gateway import JavaGateway 
 
+# Load jvm connecting instance
 jdbc_path = './target/sqream-jdbc-2.9.4-jar-with-dependencies.jar'
 gateway = JavaGateway.launch_gateway(classpath=jdbc_path)
-jvm = gateway.jvm
-jvm.Class.forName("com.sqream.jdbc.SQDriver")
 
+# Load SQream JDBC Driver
+gateway.jvm.Class.forName("com.sqream.jdbc.SQDriver")
+
+# Open a connection
 jdbc_uri = 'jdbc:Sqream://127.0.0.1:5000/master;user=sqream;password=sqream' 
 # 'jdbc:Sqream://127.0.0.1:5000/master;user=sqream;password=sqream;cluster=false;ssl=false;service=bobo'
+conn = gateway.jvm.java.sql.DriverManager.getConnection(jdbc_uri, "sqream", "sqream")
 
-conn = jvm.java.sql.DriverManager.getConnection(jdbc_uri, "sqream", "sqream")
-
+# Execute a statement
 stmt = conn.createStatement()
 rs = stmt.executeQuery('select * from t_int')
 while rs.next():

2347facb62421048164e3844b15d64b65f6c5173
Alex Karpov <alexk@sqreamtech.com>
2019-12-17 13:14:18 +0200

    Create json parser

--- src/main/java/com/sqream/jdbc/connector/JsonParser.java
+++ src/main/java/com/sqream/jdbc/connector/JsonParser.java
@@ -3,7 +3,6 @@ package com.sqream.jdbc.connector;
 import com.eclipsesource.json.Json;
 import com.eclipsesource.json.JsonArray;
 import com.eclipsesource.json.JsonObject;
-import com.eclipsesource.json.JsonValue;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,15 +11,6 @@ public class JsonParser {
 
     private static final String QUERY_TYPE = "queryType";
     private static final String QUERY_TYPE_NAMED = "queryTypeNamed";
-    private static final int TEXT_ITEM_SIZE = 100_000;
-
-    public ConnectionStateDto toConnectionState(String body) {
-        JsonObject jsonObj = parseJson(body);
-        int connId = jsonObj.get("connectionId").asInt();
-        String varcharEncoding = jsonObj.getString("varcharEncoding", "ascii");
-        varcharEncoding = (varcharEncoding.contains("874"))? "cp874" : "ascii";
-        return new ConnectionStateDto(connId, varcharEncoding);
-    }
 
     public List<ColumnMetadataDto> toQueryTypeInput(String body) {
         return toQueryType(body, QUERY_TYPE);
@@ -28,37 +18,6 @@ public class JsonParser {
 
     public List<ColumnMetadataDto> toQueryTypeOut(String body) {
         return toQueryType(body, QUERY_TYPE_NAMED);
-    }
-
-    public FetchMetadataDto toFetchMetadata(String body) {
-        JsonObject jsonObj = parseJson(body);
-        int newRowsFetched = jsonObj.get("rows").asInt();
-        JsonArray jsonSizes = jsonObj.get("colSzs").asArray();
-        int[] sizes = new int[jsonSizes.size()];
-        for (int i = 0; i < jsonSizes.size(); i++) {
-            sizes[i] = jsonSizes.get(i).asInt();
-        }
-        return new FetchMetadataDto(newRowsFetched, sizes);
-    }
-
-    public int toStatementId(String body) {
-        return parseJson(body).get("statementId").asInt();
-    }
-
-    public StatementStateDto toStatementState(String body) throws ConnException {
-        JsonObject jsonObj = parseJson(body);
-
-        if(jsonObj.get("error") != null) {
-            throw new ConnException(jsonObj.get("error").asString());
-        }
-
-        int listenerId = jsonObj.get("listener_id").asInt();
-        int port = jsonObj.get("port").asInt();
-        int portSsl = jsonObj.get("port_ssl").asInt();
-        boolean reconnect = jsonObj.get("reconnect").asBoolean();
-        String ip = jsonObj.get("ip").asString();
-
-        return new StatementStateDto(listenerId, port, portSsl, reconnect, ip);
     }
 
     private List<ColumnMetadataDto> toQueryType(String body, String type) {
@@ -81,7 +40,7 @@ public class JsonParser {
         boolean nullable = jsonObj.get("nullable").asBoolean();
         JsonArray type = jsonObj.get("type").asArray();
         String valueType = type.get(0).asString();
-        int valueSize = type.get(1).asInt() != 0 ? type.get(1).asInt() : TEXT_ITEM_SIZE;
+        int valueSize = type.get(1).asInt();
 
         return new ColumnMetadataDto(truVarchar, name, nullable, valueType, valueSize);
     }

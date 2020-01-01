e11b9988fc2a37e8d3a67f5f182379b76659e0a5
Alex Karpov <alexk@sqreamtech.com>
2020-01-01 18:46:18 +0200

    Add byte readers

---
+++ src/main/java/com/sqream/jdbc/connector/FtUbyteReader.java
@@ -0,0 +1,30 @@
+package com.sqream.jdbc.connector;
+
+import java.nio.ByteBuffer;
+
+public class FtUbyteReader implements ByteReader {
+    @Override
+    public Integer readInt(ByteBuffer buffer, int rowIndex) {
+        return buffer.get(rowIndex) & 0xFF;
+    }
+
+    @Override
+    public Short readShort(ByteBuffer buffer, int rowIndex) {
+        return (short) (buffer.get(rowIndex) & 0xFF);
+    }
+
+    @Override
+    public Long readLong(ByteBuffer buffer, int rowIndex) {
+        return (long) buffer.get(rowIndex) & 0xFF;
+    }
+
+    @Override
+    public Float readFloat(ByteBuffer buffer, int rowIndex) {
+        return (float) (buffer.get(rowIndex) & 0xFF);
+    }
+
+    @Override
+    public Double readDouble(ByteBuffer buffer, int rowIndex) {
+        return (double) (buffer.get(rowIndex) & 0xFF);
+    }
+}

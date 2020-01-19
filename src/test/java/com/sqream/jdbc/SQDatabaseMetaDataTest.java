d41ed0252a6d7080daa21ead9782e6e88a5ef484
Alex Karpov <alexk@sqreamtech.com>
2020-01-19 16:11:34 +0200

    Added tests for SQDatabaseMetaData

---
+++ src/test/java/com/sqream/jdbc/SQDatabaseMetaDataTest.java
@@ -0,0 +1,83 @@
+package com.sqream.jdbc;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.script.ScriptException;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+
+import static org.junit.Assert.*;
+
+public class SQDatabaseMetaDataTest {
+    private static SQDatabaseMetaData metadata;
+
+    @Before
+    public void setUp() throws KeyManagementException, ScriptException, NoSuchAlgorithmException,
+            Connector.ConnException, IOException, SQLException {
+
+        String user = "sqream";
+        String catalog = "master";
+        Connector connector = new Connector("127.0.0.1", 5000, false, false);
+        SQConnection connection = new SQConnection(connector);
+        metadata = new SQDatabaseMetaData(connector, connection, user, catalog);
+    }
+
+    @Test
+    public void storesLowerCaseIdentifiersTest() throws IOException, SQLException, ScriptException, NoSuchAlgorithmException, Connector.ConnException, KeyManagementException {
+        assertNotNull(metadata);
+        // SQ-1299
+        assertTrue(metadata.storesLowerCaseIdentifiers());
+    }
+
+    @Test
+    public void storesLowerCaseQuotedIdentifiersTest() throws SQLException {
+        assertNotNull(metadata);
+        // SQ-1299
+        assertFalse(metadata.storesLowerCaseQuotedIdentifiers());
+    }
+
+    @Test
+    public void storesUpperCaseIdentifiersTest() throws SQLException {
+        assertNotNull(metadata);
+        // SQ-1299
+        assertFalse(metadata.storesUpperCaseIdentifiers());
+    }
+
+    @Test
+    public void storesUpperCaseQuotedIdentifiersTest() throws SQLException {
+        assertNotNull(metadata);
+        // SQ-1299
+        assertFalse(metadata.storesUpperCaseQuotedIdentifiers());
+    }
+
+    @Test
+    public void storesMixedCaseIdentifiersTest() throws SQLException {
+        assertNotNull(metadata);
+        // SQ-1299
+        assertFalse(metadata.storesMixedCaseIdentifiers());
+    }
+
+    @Test
+    public void storesMixedCaseQuotedIdentifiersTest() throws SQLException {
+        assertNotNull(metadata);
+        // SQ-1299
+        assertTrue(metadata.storesMixedCaseQuotedIdentifiers());
+    }
+
+    @Test
+    public void supportsMixedCaseIdentifiersTest() throws SQLException {
+        assertNotNull(metadata);
+        // SQ-1299
+        assertFalse(metadata.supportsMixedCaseIdentifiers());
+    }
+
+    @Test
+    public void supportsMixedCaseQuotedIdentifiersTest() throws SQLException {
+        assertNotNull(metadata);
+        // SQ-1299
+        assertTrue(metadata.supportsMixedCaseQuotedIdentifiers());
+    }
+}

d421332b7a281ca53869ecd5e98d2fc6767ba082
Alex Karpov <alexk@sqreamtech.com>
2020-01-19 15:47:27 +0200

    Fix comment for test

--- src/test/java/com/sqream/jdbc/SQResultSetMetaDataTest.java
+++ src/test/java/com/sqream/jdbc/SQResultSetMetaDataTest.java
@@ -17,10 +17,10 @@ public class SQResultSetMetaDataTest {
         String catalog = "master";
         Connector connector = new Connector("127.0.0.1", 5000, false, false);
 
-        // SQ-1246: JDBC - Set default value to isCurrency
-        // check that return false by default
         SQResultSetMetaData resultSetMetaData = new SQResultSetMetaData(connector, catalog);
 
+        // SQ-1246: JDBC - Set default value to isCurrency
+        // check that return false by default
         assertFalse(resultSetMetaData.isCurrency(1));
     }
 

3dfa8bcc4f610362b4fc3b4a1696ec50e4923fea
Alex Karpov <alexk@sqreamtech.com>
2020-02-27 16:19:22 +0200

    Remove print in console

--- src/main/java/com/sqream/jdbc/SQConnection.java
+++ src/main/java/com/sqream/jdbc/SQConnection.java
@@ -31,7 +31,6 @@ public class SQConnection implements Connection {
 
 	private Vector<SQStatement> Statement_list = new Vector<>();
 	private Connector globalClient;
-	private boolean printouts = false;
 	private SQDatabaseMetaData data = null;
 	private ConnectionParams params;
 	private AtomicBoolean isClosed = new AtomicBoolean(true);
@@ -180,7 +179,6 @@ public class SQConnection implements Connection {
 			throw new SQLException(MessageFormat.format("Statement is closed: isClosed=[{0}]", isClosed));
 		}
 
-		if (printouts) System.out.println("prepareStatement5");
 		//spark use this function 
 		//sql = sql.replace("\"", "");
 		SQPreparedStatement SQPS = null;
@@ -209,19 +207,18 @@ public class SQConnection implements Connection {
 			isClosed.set(true);
 
 		} catch (IOException | ScriptException | ConnException e) {
-			// TODO Auto-generated catch block
 			throw new SQLException(e);
 		}
 	}
 	
 	@Override
 	public DatabaseMetaData getMetaData() throws SQLException {
-		log("inside getMetaData SQConnection");
+		LOGGER.log(Level.FINE, "get DatabaseMetaData");
+
 		if (data == null) {
 			try {
 				data = new SQDatabaseMetaData(globalClient,this, params.getUser(), params.getDbName());
 			} catch (Exception e) {
-				e.printStackTrace();
 				throw new SQLException(e);
 			}
 		}
@@ -230,29 +227,30 @@ public class SQConnection implements Connection {
 	
 	@Override
 	public boolean isClosed() throws SQLException {
-		LOGGER.log(Level.FINE, MessageFormat.format("isClosed return [{0}]", isClosed.get()));
+		LOGGER.log(Level.FINE, MessageFormat.format("isClosed returns [{0}]", isClosed.get()));
 		return isClosed.get();
 	}
 	
 	@Override
 	public void commit() throws SQLException {
-		log("inside commit SQConnection");
+		LOGGER.log(Level.FINE, "commit");
 	}
 	
 	@Override
 	public boolean getAutoCommit() throws SQLException {
-		log("inside getAutoCommit SQConnection");
+		LOGGER.log(Level.FINE, "getAutoCommit");
 		return true;
 	}
 	
 	@Override
 	public String getSchema() {
+		LOGGER.log(Level.FINE, MessageFormat.format("Schema=[{0}]", params.getSchema()));
 		return params.getSchema();
 	}
 	
 	@Override
 	public String getCatalog() throws SQLException {
-		log("inside getCatalog SQConnection");
+		LOGGER.log(Level.FINE, MessageFormat.format("DBName = [{0}]", params.getDbName()));
 		return params.getDbName();
 	}
 
@@ -287,46 +285,29 @@ public class SQConnection implements Connection {
 
 	@Override
 	public boolean isValid(int timeout) throws SQLException {
-		log("inside isValid SQConnection");
+		LOGGER.log(Level.FINE, MessageFormat.format(
+				"Params: timeout=[{0}]. Return [{1}]", timeout, globalClient.isOpen()));
 		return globalClient.isOpen();
 	}
 	
 	@Override
 	public void setAutoCommit(boolean autoCommit) throws SQLException {
 		log("inside setAutoCommit SQConnection");
-		if (printouts) {
-			System.out.println("setAutoCommit");
-		}
 	}
 
 	@Override
 	public void setCatalog(String catalog) throws SQLException {
 		log("inside setCatalog SQConnection");
-		// TODO Shirlie
-		// / _connectionInfo.put("dbname", catalog);
-		if (printouts) System.out.println("setCatalog");
 	}
 
 	@Override
 	public void setClientInfo(Properties properties) throws SQLClientInfoException {
-		try {
-			log("inside setClientInfo SQConnection");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		if (printouts) System.out.println("setCatalog");
+		log("inside setClientInfo SQConnection");
 	}
 
 	@Override
 	public void setClientInfo(String name, String value) throws SQLClientInfoException {
-		try {
-			log("inside setClientInfo SQConnection");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		if (printouts) System.out.println("setClientInfo2");
+		log("inside setClientInfo SQConnection");
 	}
 
 	@Override
@@ -334,23 +315,17 @@ public class SQConnection implements Connection {
 		/* Puts this connection in read-only mode as a hint 
 		 * to the driver to enable database optimizations.  */
 		log("inside setReadOnly SQConnection");
-		if (printouts) System.out.println("setReadOnly");
-		//throw new SQLFeatureNotSupportedException();
 	}
 	
 	@Override
 	public int getNetworkTimeout() throws SQLException {
 		log("inside getNetworkTimeout SQConnection");
-		if (printouts) System.out.println("getNetworkTimeout");
 		return 0;
 	}
 	
 	@Override
 	public void setNetworkTimeout(Executor executor, int milliseconds) throws SQLException {
 		log("inside setNetworkTimeout SQConnection");
-		if (printouts) {
-			System.out.println("setNetworkTimeout");
-		}
 	}
 	
 	@Override
@@ -371,7 +346,7 @@ public class SQConnection implements Connection {
 //		System.out.println("clearWarnings");
 	}
 
-	private void log(String line) throws SQLException {
+	private void log(String line) {
 		LOGGER.log(Level.FINE, line);
 	}
 
@@ -389,162 +364,136 @@ public class SQConnection implements Connection {
 	@Override
 	public void setHoldability(int holdability) throws SQLException {
 		log("inside setHoldability SQConnection");
-		if (printouts) System.out.println("setHoldability");
 		throw new SQLFeatureNotSupportedException("unwrap in SQConnection");
 	}
 	
 	@Override
 	public Savepoint setSavepoint() throws SQLException {
 		log("inside setSavepoint SQConnection");
-		if (printouts) System.out.println("setSavepoint");
 		throw new SQLFeatureNotSupportedException("setSavepoint in SQConnection");
 	}
 
 	@Override
 	public Savepoint setSavepoint(String name) throws SQLException {
 		log("inside setSavepoint 2 SQConnection");
-		if (printouts) System.out.println("setSavepoint");
 		throw new SQLFeatureNotSupportedException("setSavepoint 2 in SQConnection");
 	}
 
 	@Override
 	public void setTransactionIsolation(int level) throws SQLException { 
 		log("inside setTransactionIsolation SQConnection");
-		if (printouts) System.out.println("setTransactionIsolation");
 		throw new SQLFeatureNotSupportedException("setTransactionIsolation in SQConnection");
 	}
 
 	@Override
 	public void setTypeMap(Map<String, Class<?>> map) throws SQLException {	
 		log("inside setTypeMap SQConnection");
-		if (printouts) System.out.println("setTypeMap");
 		throw new SQLFeatureNotSupportedException("setTypeMap in SQConnection");
 	}
 
 	@Override
 	public void abort(Executor executor) throws SQLException {
 		log("inside abort SQConnection");
-		if (printouts) System.out.println("abort");
 		throw new SQLFeatureNotSupportedException("abort in SQConnection");
 	}
 
 	@Override
 	public void setSchema(String schema) throws SQLException {
-		if (printouts) System.out.println("setSchema");
 		throw new SQLFeatureNotSupportedException("setSchema in SQConnection");
 	}
 	
 	@Override
 	public PreparedStatement prepareStatement(String sql, int resultSetType, int resultSetConcurrency, int resultSetHoldability) throws SQLException {
-		if (printouts) System.out.println("prepareStatement6");
 		throw new SQLFeatureNotSupportedException("prepareStatement in SQConnection");
 	}
 
 	@Override
 	public void releaseSavepoint(Savepoint savepoint) throws SQLException {
-		if (printouts) System.out.println("releaseSavepoint");
 		throw new SQLFeatureNotSupportedException("releaseSavepoint in SQConnection");
 	}
 
 	@Override
 	public void rollback() throws SQLException {
-		if (printouts) System.out.println("rollback");
 		throw new SQLFeatureNotSupportedException("rollback in SQConnection");
 	}
 
 	@Override
 	public void rollback(Savepoint savepoint) throws SQLException {
-		if (printouts) System.out.println("rollback2");
 		throw new SQLFeatureNotSupportedException("rollback in SQConnection");
 	}
 	
 	@Override
 	public PreparedStatement prepareStatement(String sql, int autoGeneratedKeys) throws SQLException {
-		// System.out.println("prepareStatement2");
 		throw new SQLFeatureNotSupportedException("prepareStatement 2 in SQConnection");
 	}
 
 	@Override
 	public PreparedStatement prepareStatement(String sql, int[] columnIndexes) throws SQLException {
-		// System.out.println("prepareStatement3");
 		throw new SQLFeatureNotSupportedException("prepareStatement 3 in SQConnection");
 	}
 
 	@Override
 	public PreparedStatement prepareStatement(String sql, String[] columnNames) throws SQLException {
-		if (printouts) System.out.println("prepareStatement4");
 		throw new SQLFeatureNotSupportedException("prepareStatement 4 in SQConnection");
 	}
 	
 	@Override
 	public String nativeSQL(String sql) throws SQLException {
-		// System.out.println("nativeSQL");
 		throw new SQLFeatureNotSupportedException("nativeSQL in SQConnection");
 	}
 
 	@Override
 	public CallableStatement prepareCall(String sql) throws SQLException {
-		// System.out.println("prepareCall");
 		throw new SQLFeatureNotSupportedException("prepareCall in SQConnection");
 	}
 
 	@Override
 	public CallableStatement prepareCall(String sql, int resultSetType, int resultSetConcurrency) throws SQLException {
-		// System.out.println("prepareCall2");
 		throw new SQLFeatureNotSupportedException("prepareCall 2 in SQConnection");
 	}
 
 	@Override
 	public CallableStatement prepareCall(String sql, int resultSetType, int resultSetConcurrency, int resultSetHoldability) throws SQLException {
-		// System.out.println("prepareCall3");
 		throw new SQLFeatureNotSupportedException("prepareCall 3 in SQConnection");
 	}
 	
 	@Override
 	public Map<String, Class<?>> getTypeMap() throws SQLException {
-		// System.out.println("getTypeMap");
 		throw new SQLFeatureNotSupportedException("getTypeMap in SQConnection");
 	}
 	
 	@Override
 	public String getClientInfo(String name) throws SQLException {
-//		System.out.println("getClientInfo2");
 		throw new SQLFeatureNotSupportedException("getClientInfo in SQConnection");
 	}
 	
 	@Override
 	public Struct createStruct(String typeName, Object[] attributes) throws SQLException {
-//		System.out.println("createStruct");
 		throw new SQLFeatureNotSupportedException("createStruct in SQConnection");
 	}
 	
 	@Override
 	public Array createArrayOf(String typeName, Object[] elements) throws SQLException {
-//		System.out.println("createArrayOf");
 		throw new SQLFeatureNotSupportedException("createArrayOf in SQConnection");
 	}
 
 	@Override
 	public Blob createBlob() throws SQLException {
-//		System.out.println("createBlob");
 		throw new SQLFeatureNotSupportedException("createBlob in SQConnection");
 	}
 
 	@Override
 	public Clob createClob() throws SQLException {
-//		System.out.println("createClob");
 		throw new SQLFeatureNotSupportedException("createClob in SQConnection");
 	}
 
 	@Override
 	public NClob createNClob() throws SQLException {
-//		System.out.println("createNClob");
 		throw new SQLFeatureNotSupportedException("createNClob in SQConnection");
 	}
 
 	@Override
 	public SQLXML createSQLXML() throws SQLException {
-//		System.out.println("createSQLXML");
 		throw new SQLFeatureNotSupportedException("createSQLXML in SQConnection");
 	}
 
